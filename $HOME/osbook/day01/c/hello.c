f0176b3541de222fd409b15beab081814dad631e
yttnn <67893139+yttnn@users.noreply.github.com>
2023-01-20 00:01:56 +0900

    fix

--- $HOME/osbook/day01/c/hello.c
+++
@@ -1,5 +0,0 @@
-EFI_STATUS EfiMain(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-  SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello, world!\n");
-  while(1);
-  return 0;
-}
