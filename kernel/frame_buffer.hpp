349ae47e73160b7fdd6b1c614d4472e595235d07
yttnn <67893139+yttnn@users.noreply.github.com>
2023-02-25 14:55:41 +0900

    day10f

--- kernel/frame_buffer.hpp
+++ kernel/frame_buffer.hpp
@@ -13,6 +13,7 @@ class FrameBuffer {
     Error Initialize(const FrameBufferConfig& config);
     Error Copy(Vector2D<int> dst_pos, const FrameBuffer& src, const Rectangle<int>& src_area);
     void Move(Vector2D<int> dst_pos, const Rectangle<int>& src);
+    const FrameBufferConfig& Config() const { return config_; }
 
     FrameBufferWriter& Writer() { return *writer_; }
   
--- kernel/layer.hpp
+++ kernel/layer.hpp
@@ -38,9 +38,11 @@ class LayerManager {
     void MoveRelative(unsigned int id, Vector2D<int> pos_diff);
     void UpDown(unsigned int id, int new_height);
     void Hide(unsigned int id);
+    Layer* FindLayerByPosition(Vector2D<int> pos, unsigned int exclude_id) const;
 
   private:
     FrameBuffer* screen_{nullptr};
+    mutable FrameBuffer back_buffer_{};
     std::vector<std::unique_ptr<Layer>> layers_{};
     std::vector<Layer*> layer_stack_{};
     unsigned int latest_id_{0};
