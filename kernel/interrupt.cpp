3fc047b89620b69760857c666ce93dd43a46799e
yttnn <67893139+yttnn@users.noreply.github.com>
2023-02-25 23:40:23 +0900

    day11c

--- kernel/interrupt.cpp
+++ kernel/interrupt.cpp
@@ -1,6 +1,7 @@
 #include "interrupt.hpp"
 #include "asmfunc.h"
 #include "segment.hpp"
+#include "timer.hpp"
 
 std::array<InterruptDescriptor, 256> idt;
 
@@ -28,7 +29,7 @@ namespace {
 
   __attribute__((interrupt))
   void IntHandlerLAPICTimer(InterruptFrame* frame) {
-    msg_queue->push_back(Message{Message::kInterruptLAPICTimer});
+    LAPICTimerOnInterrupt();
     NotifyEndOfInterrupt();
   }
 }
